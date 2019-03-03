626454dd0066b72101bfff48d4186247e6705a0f
Steven <stevengu0119@outlook.com>
2019-03-02 20:36:31 -0500

    Remove unicode from comments

--- src/solver/BacktrackingSearchSolver.java
+++ src/solver/BacktrackingSearchSolver.java
@@ -22,12 +22,12 @@ public class BacktrackingSearchSolver extends Solver{
 	/*
 	function BACKTRACK(assignment, csp) returns a solution, or failure
 	if assignment is complete then return assignment
-	var ¡û SELECT-UNASSIGNED-VARIABLE(csp)
+	var <- SELECT-UNASSIGNED-VARIABLE(csp)
 	for each value in ORDER-DOMAIN-VALUES(var, assignment, csp) do
 	if value is consistent with assignment then
 	add {var = value} to assignment
 	result =  backtrack(assignment, csp)
-	if result = failure then
+	if result = failure then
 	return result
 	remove {var = value} and inferences from assignment
 	return failure
